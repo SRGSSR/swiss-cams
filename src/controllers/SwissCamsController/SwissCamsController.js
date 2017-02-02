c2af0beb2699a66455f6622eaeb89163448e5813
rbens <rabah.benseghir@gmail.com>
2017-02-02 13:35:26 +0100

    add search for cam provider

--- src/controllers/SwissCamsController/SwissCamsController.js
+++ src/controllers/SwissCamsController/SwissCamsController.js
@@ -1,3 +1,16 @@
-angular.module('swisscams').controller('SwissCamsController', function() {
-    
+angular.module('swisscams').controller('SwissCamsController', function(camProvider) {
+
+    var params = { region : 'Switzerland', bestshot : '0', randomize : '0', size : 'archiveprev', limit : 20};
+    var camProviderPromise  = camProvider.search(params);
+    var metadata;
+
+    //example of call api metadata
+    if(camProviderPromise){
+        camProviderPromise.then(function success(response){
+            if(response.data){
+                metadata = response.data;
+            }
+        });
+    }
+
 });
